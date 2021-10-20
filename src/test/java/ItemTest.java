190fb5e76d3f6d8c4b2b625e8ac12d4999a30a6e
SreehariTS03 <sreeharisudarshan@gmail.com>
2021-10-20 20:25:08 +0530

    Part 3: Failing test case

---
+++ src/test/java/ItemTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemTest {
+
+    @Test
+    public void return_total_order_value_of_selected_items_from_menu(){
+
+        Restaurant restaurant;
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant =new Restaurant("Luigi's Pizza","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Burger",69);
+        restaurant.addToMenu("Coke",29);
+        restaurant.addToMenu("Veg Pizza",119);
+        restaurant.addToMenu("Corn Pizza", 159);
+        restaurant.addToMenu("Cheese Pizza",209);
+
+        List<Item> order = new ArrayList<Item>();
+        restaurant.addToOrder("Cheese Pizza",209);
+        restaurant.addToOrder("Coke",29);
+
+
+        assertEquals(278,restaurant.getOrderTotalValue());
+
+
+
+    }
+}
